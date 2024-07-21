71ddc5937f510f3b9a706a4bf25f2b09f09148ef
harsh44322 <harsh88588@gmail.com>
2024-07-21 21:06:05 +0530

    script.js commit

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
+// -------------------------------------------------------------------------------------
+// Hoisting...
+// -------------------------------------------------------------------------------------
+
+
 // var Hworld = "Hello World";
 
 // console.log(Hworld);
-getName();
+// getName();
 // console.log(x);
-console.log(getName1);
-console.log(getName);
-console.log(getName2);
+// console.log(getName1);
+// console.log(getName);
+// console.log(getName2);
 
 // var x = 9;
 
-function getName(){
-    console.log("Hello World");
-}
+// function getName(){
+//     console.log("Hello World");
+// }
 
-var getName1 = () => {
-    console.log("This is arrow function");
-}
+// var getName1 = () => {
+//     console.log("This is arrow function");
+// }
 
-var getName2 = function(){
-    console.log("function 2")
-}
+// var getName2 = function(){
+//     console.log("function 2")
+// }
 // getName();
 // console.log(x);
-console.log(getName1);
-console.log(getName);
-console.log(getName2);
+// console.log(getName1);
+// console.log(getName);
+// console.log(getName2);
+
+// -------------------------------------------------------------------------------------
+// Working of functions
+// -------------------------------------------------------------------------------------
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a() {
+    var x = 10;
+    console.log(x);
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+}
