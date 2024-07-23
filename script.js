1a450aa1f41bead72647859bc23e01ead744ed68
harsh44322 <harsh88588@gmail.com>
2024-07-23 14:19:30 +0530

    script.js commit

--- script.js
+++ script.js
@@ -86,5 +86,18 @@
 // const b = 100;
 
 // -------------------------------------------------------------------------------------
-// 
+// Block, Block Scope and Shadowing
 // -------------------------------------------------------------------------------------
+
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a); //10
+    console.log(b); //20
+    console.log(c); //30
+}
+console.log(a); //10
+console.log(b); //Uncaught ReferenceError: b is not defined
+console.log(c);
