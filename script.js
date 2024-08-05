afedb8647a4267c5b8269bbc3c55b8cc0d3254a6
harsh44322 <harsh88588@gmail.com>
2024-08-05 16:05:19 +0530

    script.js commit

--- script.js
+++ script.js
@@ -35,20 +35,20 @@
 // Working of functions
 // -------------------------------------------------------------------------------------
 
-var x = 1;
-a();
-b();
-console.log(x);
+// var x = 1;
+// a();
+// b();
+// console.log(x);
 
-function a() {
-    // var x = 10;
-    console.log(x);
-}
+// function a() {
+//     // var x = 10;
+//     console.log(x);
+// }
 
-function b() {
-    var x = 100;
-    console.log(x);
-}
+// function b() {
+//     var x = 100;
+//     console.log(x);
+// }
 
 
 // -------------------------------------------------------------------------------------
