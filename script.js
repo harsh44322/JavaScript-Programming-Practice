b0fdf43df757fd726f8a1165bdbf5f493396277e
harsh44322 <harsh88588@gmail.com>
2024-08-08 22:49:16 +0530

    script.js commit

--- script.js
+++ script.js
@@ -91,17 +91,17 @@
 // -------------------------------------------------------------------------------------
 
 
-{
-    var a = 10;
-    let b = 20;
-    const c = 30;
-    console.log(a); //10
-    console.log(b); //20
-    console.log(c); //30
-}
-console.log(a); //10
-console.log(b); //Uncaught ReferenceError: b is not defined - because b and c are block scoped.
-console.log(c);
+// {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+//     console.log(a); //10
+//     console.log(b); //20
+//     console.log(c); //30
+// }
+// console.log(a); //10
+// console.log(b); //Uncaught ReferenceError: b is not defined - because b and c are block scoped.
+// console.log(c);
 
 // -------------------------------------------------------------------------------------
 // Shadowing and Illegal Shadowing
